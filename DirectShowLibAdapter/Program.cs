021484eb69c5a7284f7fd81c6213eef6eb413415
Ovis Maximus <Ovis.Maximus@gmail.com>
2022-10-10 06:31:58 +0200

    implement simple hardware description CLI

---
+++ DirectShowLibAdapter/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace DirectShowLibAdapter;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var deviceController = new DirectShowDeviceAdapterImpl();
+        var devicesList = deviceController.GetCameraDevicesList();
+        foreach (var cameraDevice in devicesList)
+        {
+            Console.WriteLine(cameraDevice.GetDeviceName());
+            foreach(var property in cameraDevice.GetPropertiesList())
+            {
+                Console.WriteLine($"        {property}");
+            }
+        }
+    }    
+}
+
