75070f9c7e594a3eb27b7d6c2112ecd4ddc3d854
Ovis Maximus <Ovis.Maximus@gmail.com>
2022-10-23 21:22:39 +0200

    fix namespace and prevent null dereference in case of unintended usage.

--- RestoreWebCamConfig/CameraAdapter/CameraDto.cs
+++ RestoreWebCamConfig/CameraAdapter/CameraDto.cs
@@ -1,32 +1,41 @@
 using System.Text;
 
-namespace RestoreWebCamConfig;
+namespace RestoreWebCamConfig.CameraAdapter;
 
-    public record CameraDto(string Name)
+public record CameraDto(string Name)
+{
+    public string Name { get; set; } = Name;
+    public IList<CameraPropertyDto>? Properties { get; set; }
+
+    public override string ToString()
     {
-        public string Name { get; set; } = Name;
-        public IList<CameraPropertyDto>? Properties { get; set; }
-
-        public override string ToString()
-        {
-            var result = new StringBuilder("Camera ");
-            var delimeter = "";
-            result.Append(Name);
-            result.Append(": ");
-            foreach (var property in Properties)
-            {
-                result.Append(delimeter);
-                result.Append(property.Name);
-                result.Append("=");
-                result.Append(property.Value);
-                if (property.CanAdaptAutomatically)
-                {
-                    result.Append(" (");
-                    result.Append(property.IsAutomaticallyAdapting ? "auto": "manual");
-                    result.Append(')');
-                }
-                delimeter = ", ";
-            }
-            return result.ToString();
-        }
+        var result = new StringBuilder("Camera ");
+        result.Append(Name);
+        result.Append(": ");
+        result.Append(Properties == null ? "no properties set." : RenderPropertyDescription(Properties));
+        return result.ToString();
     }
+
+    private string RenderPropertyDescription(IList<CameraPropertyDto> properties)
+    {
+        StringBuilder result = new StringBuilder();
+        var delimiter = "";
+        foreach (var property in properties)
+        {
+            result.Append(delimiter);
+            result.Append(property.Name);
+            result.Append("=");
+            result.Append(property.Value);
+            if (property.CanAdaptAutomatically)
+            {
+                result.Append(" (");
+                result.Append(property.IsAutomaticallyAdapting ? "auto" : "manual");
+                result.Append(')');
+            }
+
+            delimiter = ", ";
+        }
+
+        return result.ToString();
+    }
+}
