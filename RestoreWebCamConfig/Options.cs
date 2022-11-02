2e8bbd7bd27eafb9cb10c1519cdd089cfe7c7dbd
Ovis Maximus <Ovis.Maximus@gmail.com>
2022-11-02 21:09:21 +0100

    Implement command line parsing

--- RestoreWebCamConfig/Options.cs
+++ RestoreWebCamConfig/Options.cs
@@ -4,4 +4,5 @@ public record Options
 {
     public string? CameraName { get; set; }
     public string? FileName { get; set; }
+    public bool IsHelpRequested { get; set; }
 }
--- Tests/ApplicationTests.cs
+++ Tests/ApplicationTests.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Collections.Generic;
+using NDesk.Options;
 using RestoreWebCamConfig;
 using Xunit;
 
@@ -6,19 +7,73 @@ namespace Tests;
 
 public class ApplicationTests
 {
-    [Fact]
-    public void blah()
+    [Theory]
+    [InlineData(new[] { "-f", "theFileName.txt" }, "theFileName.txt", null, false)]
+    [InlineData(new[] { "--file", "theOtherFileName.txt" }, "theOtherFileName.txt", null, false)]
+    [InlineData(new[] { "-c", "theCamName", "--help" }, null, "theCamName", true)]
+    [InlineData(new[] { "-h", "--camera", "theOtherCamName" }, null, "theOtherCamName", true)]
+    [InlineData(new[] { "--cam", "theThirdCamName" }, null, "theThirdCamName", false)]
+    [InlineData(new[] { "-c", "theFourthCamName", "-?", "--file", "f.t" }, "f.t", "theFourthCamName", true)]
+    public void TestParameterOptions(string[] argumentList, string expectedFileName, string expectedCameraName, bool isHelpRequested)
     {
-        var options = new Options();
-        var parser = CommandLineParser.getParserWritingInto(options);
+        var parser = CommandLineParser.GetCommandLineParserFor(argumentList);
+        Assert.Equal(expectedFileName, parser.GetFileName());
+        Assert.Equal(expectedCameraName, parser.GetCameraName());
+        Assert.Equal(isHelpRequested, parser.IsHelpRequested());
+        Assert.Empty(parser.GetCommandList());
     }
 
+    [Fact]
+    public void TestCommands()
+    {
+        CommandLineParser parser = CommandLineParser.GetCommandLineParserFor(
+            new[] { "-c", "aCamName", "command1", "-f", "aFileName", "command2" });
+        Assert.Equal(2, parser.GetCommandList().Count);
+        Assert.Equal("command1", parser.GetCommandList()[0]);
+        Assert.Equal("command2", parser.GetCommandList()[1]);
+    }
 }
 
 public class CommandLineParser
 {
-    public static CommandLineParser getParserWritingInto(Options options)
+    private readonly Options _options;
+    private readonly List<string> _commands;
+
+    private CommandLineParser(OptionSet optionSet, Options options, IEnumerable<string> commandLineArguments)
     {
-        throw new NotImplementedException();
+        _options = options;
+        _commands = optionSet.Parse(commandLineArguments);
+    }
+
+    public static CommandLineParser GetCommandLineParserFor(string[] commandLineArguments)
+    {
+        var options = new Options();
+        var optionSet = new OptionSet
+        {
+            { "file|f=", "name of the file to work with", value => { options.FileName = value; }},
+            { "cam|camera|c=", "name of the camera device to work with", value => { options.CameraName = value; }},
+            { "?|help|h", "dump this help text to the console", _ => { options.IsHelpRequested = true; }}
+        };
+        return new CommandLineParser(optionSet, options, commandLineArguments);
+    }
+
+    public IReadOnlyList<string> GetCommandList()
+    {
+        return _commands.AsReadOnly();
+    }
+
+    public string? GetFileName()
+    {
+        return _options.FileName;
+    }
+
+    public string? GetCameraName()
+    {
+        return _options.CameraName;
+    }
+
+    public bool IsHelpRequested()
+    {
+        return _options.IsHelpRequested;
     }
 }
